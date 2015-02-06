5e5845157d360ee5a50bbafcf44716e82c010a26
zefredz <zefredz@gmail.com>
2015-02-06 15:40:30 +0100

    add a clear:cache command to the console

--- app/console.php
+++ app/console.php
@@ -1,5 +1,26 @@
 <?php
 
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+
 $console = new Symfony\Component\Console\Application('Claroline Archiver', '0.1');
+
+
+$console
+  ->register('cache:clear')
+  ->setDescription('Clears the cache')
+  ->setCode(function (InputInterface $input, OutputInterface $output) use ($app) {
+
+    $cacheDir = $app['cache.path'];
+    $finder = Finder::create()->in($cacheDir)->notName('.gitkeep');
+
+    $filesystem = new Filesystem();
+    $filesystem->remove($finder);
+
+    $output->writeln(sprintf("%s <info>success</info>", 'cache:clear'));
+});
+
 
 return $console;
