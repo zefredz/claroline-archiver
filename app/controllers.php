41fe3e395f3b5b4cdd227d6621506352dc637d91
zefredz <zefredz@gmail.com>
2015-02-06 20:32:47 +0100

    remove commented code

--- app/controllers.php
+++ app/controllers.php
@@ -2,10 +2,6 @@
 
 use Claroline\Archiver\Controller\IndexController;
 
-/*$app->match('/', function() use ($app) {
-  return $app['twig']->render('index.html.twig');
-})->bind('homepage');*/
-
 $app['homepage.controller'] = $app->share(function() use ($app) {
     return new IndexController($app);
 });
