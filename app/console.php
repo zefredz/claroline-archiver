ebb6e82faa304d7808e96698dbe6cf08680cbf75
zefredz <zefredz@gmail.com>
2015-02-06 11:58:43 +0100

    unused use statement should be avoided; commented code should not be commited

--- app/console.php
+++ app/console.php
@@ -1,12 +1,5 @@
 <?php
 
-use Symfony\Component\Console\Application;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\Finder\Finder;
-
-$console = new Application('Claroline Archiver', '0.1');
+$console = new Symfony\Component\Console\Application('Claroline Archiver', '0.1');
 
 return $console;
--- app/controllers.php
+++ app/controllers.php
@@ -1,10 +1,5 @@
 <?php
 
-// use Symfony\Component\Form\FormError;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-// use Symfony\Component\Validator\Constraints as Assert;
-
 $app->match('/', function() use ($app) {
   return 'It works!';
 })->bind('homepage');
