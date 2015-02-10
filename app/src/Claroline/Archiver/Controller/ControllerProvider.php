6f04f1d3f7b725da206ffb8bfb579365b94105e3
zefredz <zefredz@gmail.com>
2015-02-10 13:46:22 +0100

    mount ArchiverController

--- app/src/Claroline/Archiver/Controller/ControllerProvider.php
+++ app/src/Claroline/Archiver/Controller/ControllerProvider.php
@@ -4,6 +4,7 @@ namespace Claroline\Archiver\Controller;
 
 use Silex\Application;
 use Silex\ControllerProviderInterface;
+use Monolog\Logger;
 
 use Claroline\Archiver\Controller\IndexController;
 
@@ -11,14 +12,24 @@ class ControllerProvider implements ControllerProviderInterface {
 
   public function connect(Application $app) {
 
+    // creates a new controller based on the default route
+    $controllers = $app['controllers_factory'];
+
+    $app->log('Loading homepage controller', array(), Logger::DEBUG);
+
     $app['homepage.controller'] = $app->share(function() use ($app) {
         return new IndexController($app);
     });
 
-    // creates a new controller based on the default route
-    $controllers = $app['controllers_factory'];
-
     $controllers->get('/', "homepage.controller:index");
+
+    $app->log('Loading archiver controller', array(), Logger::DEBUG);
+
+    $app['archiver.controller'] = $app->share(function() use ($app) {
+        return new ArchiverController($app);
+    });
+
+    $controllers->get('/course', "archiver.controller:loadCourse");
 
     return $controllers;
   }
