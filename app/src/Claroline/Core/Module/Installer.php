11b500100a98ad170cfdfe2e0a258ee73942ae8a
zefredz <zefredz@gmail.com>
2015-02-11 21:42:43 +0100

    schema install

--- app/src/Claroline/Core/Module/Installer.php
+++ app/src/Claroline/Core/Module/Installer.php
@@ -5,5 +5,26 @@ namespace Claroline\Module;
 use Claroline\Core\ModuleInterface;
 
 class Installer {
-  
+
+  protected $app;
+
+  public function __construct( $app ) {
+
+    $this->app = $app;
+  }
+
+  public function install( \Claroline\Core\Module\ModuleInterface $module ) {
+
+    $schema = $this->app['db']->getSchemaManager()->createSchema();
+
+    $plateform = $this->app['db']->getDatabasePlatform();
+
+    $schema = $module->schema( $schema );
+
+    $createQueries = $schema->toSql($platform);
+
+    foreach ( $createQueries as $query ) {
+      $app['db']->executeQuery( $query );
+    }
+  }
 }
