61a3e393035ca38d010cdf159119eaba4d79138f
zefredz <zefredz@gmail.com>
2015-02-12 13:56:18 +0100

    declare migrations commands

--- app/console.php
+++ app/console.php
@@ -1,11 +1,35 @@
 <?php
 
+use Symfony\Component\Console;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
 
-$console = new Symfony\Component\Console\Application('Claroline Archiver', '0.1');
+use Doctrine\DBAL\Migrations\MigrationsVersion;
+use Doctrine\DBAL\Migrations\Tools\Console\Command as MigrationsCommand;
+
+$console = new Console\Application('Claroline Archiver', '0.1');
+
+$helperSet = new Console\Helper\HelperSet();
+$helperSet->set(new Console\Helper\DialogHelper(), 'dialog');
+$console->setHelperSet($helperSet);
+
+// Add Migrations commands
+$commands = array();
+$commands[] = new MigrationsCommand\ExecuteCommand();
+$commands[] = new MigrationsCommand\GenerateCommand();
+$commands[] = new MigrationsCommand\LatestCommand();
+$commands[] = new MigrationsCommand\MigrateCommand();
+$commands[] = new MigrationsCommand\StatusCommand();
+$commands[] = new MigrationsCommand\VersionCommand();
+
+// remove the "migrations:" prefix on each command name
+foreach ($commands as $command) {
+    $command->setName(str_replace('migrations:', '', $command->getName()));
+}
+
+$console->addCommands($commands);
 
 $console
   ->register('cache:clear')
@@ -24,7 +48,7 @@ $console
   ->register('installer:install')
   ->setDescription('Install the application')
   ->setCode(function (InputInterface $input, OutputInterface $output) use ($app) {
-    
+
     $cacheDir = $app['cache.path'];
     $filesystem = new Filesystem();
     $filesystem->mkdir($cacheDir);
